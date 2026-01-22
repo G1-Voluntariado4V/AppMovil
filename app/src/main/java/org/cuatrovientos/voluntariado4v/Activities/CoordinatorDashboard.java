1a4a44a8bb42c2433eee6bce40571c8dd6a64cd6
Ivan <ivancarrillovela@gmail.com>
2026-01-22 11:00:38 +0100

    refactor: Eliminar CoordinatorManagementFragment y simplificar navegación del coordinador
    - Eliminar la clase `CoordinatorManagementFragment.java` y su layout asociado `fragment_coordinator_management.xml`.
    - Actualizar `CoordinatorDashboard.java` para remover las referencias y la inicialización del fragmento de gestión.
    - Ajustar el margen inferior y eliminar el ID innecesario en una tarjeta de `fragment_coordinator_home.xml`.
    - Modificar el título del elemento de menú `navigation_activities` de "Activ." a "Actividades" en `menu_bottom_nav_coordinator.xml`.

--- app/src/main/java/org/cuatrovientos/voluntariado4v/Activities/CoordinatorDashboard.java
+++ app/src/main/java/org/cuatrovientos/voluntariado4v/Activities/CoordinatorDashboard.java
@@ -13,7 +13,6 @@ import com.google.android.material.navigation.NavigationBarView;
 
 import org.cuatrovientos.voluntariado4v.Fragments.CoordinatorActivitiesFragment;
 import org.cuatrovientos.voluntariado4v.Fragments.CoordinatorHomeFragment;
-import org.cuatrovientos.voluntariado4v.Fragments.CoordinatorManagementFragment;
 import org.cuatrovientos.voluntariado4v.Fragments.CoordinatorProfileFragment;
 import org.cuatrovientos.voluntariado4v.Fragments.CoordinatorUsersFragment;
 import org.cuatrovientos.voluntariado4v.R;
@@ -26,7 +25,6 @@ public class CoordinatorDashboard extends AppCompatActivity {
     private final Fragment homeFragment = new CoordinatorHomeFragment();
     private final Fragment usersFragment = new CoordinatorUsersFragment();
     private final Fragment activitiesFragment = new CoordinatorActivitiesFragment();
-    private final Fragment managementFragment = new CoordinatorManagementFragment();
     private final Fragment profileFragment = new CoordinatorProfileFragment();
 
     private final FragmentManager fm = getSupportFragmentManager();
@@ -41,7 +39,6 @@ public class CoordinatorDashboard extends AppCompatActivity {
 
         // Inicializar fragmentos: añadimos todos y ocultamos los que no son el Home
         fm.beginTransaction().add(R.id.coordinator_fragment_container, profileFragment, "5").hide(profileFragment).commit();
-        fm.beginTransaction().add(R.id.coordinator_fragment_container, managementFragment, "4").hide(managementFragment).commit();
         fm.beginTransaction().add(R.id.coordinator_fragment_container, activitiesFragment, "3").hide(activitiesFragment).commit();
         fm.beginTransaction().add(R.id.coordinator_fragment_container, usersFragment, "2").hide(usersFragment).commit();
         fm.beginTransaction().add(R.id.coordinator_fragment_container, homeFragment, "1").commit();
